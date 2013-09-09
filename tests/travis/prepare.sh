5e0df27855490f60ba632871c566814451d67ed0
Benaka Moorthi <benaka.moorthi@gmail.com>
2013-09-09 18:57:29 -0400

    Disable UI tests, remove debugging output and remove phantomjs installing code.

--- tests/travis/prepare.sh
+++ tests/travis/prepare.sh
@@ -9,16 +9,6 @@ if [ "$TEST_DIR" = "UI" ];
 then
     sudo sh -c "echo ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula select true | debconf-set-selections"
     sudo apt-get install -qq ttf-mscorefonts-installer
-
-    #cd tmp
-    #wget "https://phantomjs.googlecode.com/files/phantomjs-1.9.1-linux-x86_64.tar.bz2" -O phantomjs.tar.bz2
-    #tar xvjf phantomjs.tar.bz2
-    #cd phantomjs*
-    #export PATH=$PATH:`pwd`/bin
-    #cd ../..
-
-    echo "Using phantomjs version:"
-    phantomjs --version
 fi
 
 # Copy Piwik configuration
